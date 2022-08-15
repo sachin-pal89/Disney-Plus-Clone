d718680b6cab03ad15280b676e2667c6282e0133
Sachin Pal <sachinpal01012003@gmail.com>
2022-08-15 12:07:34 +0530

    Add Image Slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Viewers from './components/Viewers';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Viewers />
     </div>
   );
 }
